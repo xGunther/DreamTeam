6bca674422c7e1a28f1bcbd646c9d5f6cc2e7366
xGunther <109365935+xGunther@users.noreply.github.com>
2023-03-13 20:47:54 +0100

    push

---
+++ js/javascript.js
@@ -0,0 +1,16 @@
+class Movie{
+    var = ["The Lion King", "animation",]
+
+}
+class Artist{
+
+}
+class Director extends Artist{
+
+}
+class Actor extends Artist{
+
+}
+class Writer extends Artist{
+
+}
